8ee448582fed376335bf8158c805b792018c1f38
Jeff Rescignano <jeffresc@icloud.com>
2019-03-04 00:11:55 -0500

    v1.1

--- RedditContactPhotoProvider.m
+++ RedditContactPhotoProvider.m
@@ -4,16 +4,26 @@
 @implementation RedditContactPhotoProvider
 
 - (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification {
-  NSString *username = [[[[[notification.applicationUserInfo valueForKeyPath:@"aps.alert.title"] componentsSeparatedByString:@"u/"] objectAtIndex:1] componentsSeparatedByString:@" "] objectAtIndex:0];
-  if (!username) return nil;
-  NSData *data = [NSData dataWithContentsOfURL: [NSURL URLWithString:[NSString stringWithFormat:@"https://www.reddit.com/user/%@/about.json",username]]];
-  NSError* error;
-  NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
-  if(error) { return nil; }
-  NSString *photoURLStr = json[@"data"][@"icon_img"];
-  NSURL *photoURL = [NSURL URLWithString:photoURLStr];
-  if (!photoURL) return nil;
-  return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerDownloadingPromiseWithPhotoIdentifier:photoURLStr fromURL:photoURL];
+  NSString *notificationTitle = [notification.applicationUserInfo valueForKeyPath:@"aps.alert.title"];
+  NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:@"u/[A-Za-z0-9_-]+" options:0 error:nil];
+  long n = [regex numberOfMatchesInString:notificationTitle options:0 range:NSMakeRange(0, [notificationTitle length])];
+  if (n> 0) {
+	NSArray *matches = [regex matchesInString:notificationTitle options:0 range:NSMakeRange(0, [notificationTitle length])];
+      for (NSTextCheckingResult *match in matches) {
+        NSString *username = [notificationTitle substringWithRange:[match range]];
+		NSData *data = [NSData dataWithContentsOfURL: [NSURL URLWithString:[NSString stringWithFormat:@"https://www.reddit.com/%@/about.json",username]]];
+		NSError* error;
+		NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+		if(error) { return nil; }
+		NSString *photoURLStr = json[@"data"][@"icon_img"];
+		NSURL *photoURL = [NSURL URLWithString:photoURLStr];
+		if (!photoURL) return nil;
+		return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerDownloadingPromiseWithPhotoIdentifier:photoURLStr fromURL:photoURL];
+      }
+	  return nil;
+  } else {
+	return nil;
+  }
 }
 
 @end
